cdbe0e9d7519bd676a19660794fd9b8f4db132b0
Raga Subekti <rfilmriot@gmail.com>
2019-05-02 10:22:42 +0700

    Create AddTodo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import TodoList from "./components/TodoList";
 import TodoListDone from "./components/TodoListDone";
+import AddTodo from "./components/AddTodo";
 
 function App() {
   return (
     <div className="App">
+      <div>
+        <AddTodo />
+      </div>
       <div>
         <span>Todo List:</span>
         <TodoList />
---
+++ src/components/AddTodo.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { connect } from "react-redux";
+import { addTodo } from "../reducers";
+
+class AddTodo extends React.Component {
+  state = {
+    todo: ""
+  };
+
+  handleInputChange = e => {
+    this.setState({
+      todo: e.target.value
+    });
+  };
+
+  handleSubmit = () => {
+    if (this.state.todo.length <= 0) {
+      return alert("You need to type something!");
+    }
+    this.props.addTodo(this.state.todo);
+    this.setState({
+      todo: ""
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          onChange={this.handleInputChange}
+          placeholder="New Todo"
+          value={this.state.todo}
+        />
+        <button onClick={this.handleSubmit}>Add</button>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  null,
+  { addTodo }
+)(AddTodo);
